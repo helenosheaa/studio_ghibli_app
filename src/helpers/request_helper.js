829f69e53baa19641e10b1a2d0e56aaf16913c34
helenosheaa <helenosheaa@gmail.com>
2018-08-12 10:38:38 +0100

    initial commit

---
+++ src/helpers/request_helper.js
@@ -0,0 +1,11 @@
+const RequestHelper = function (url) {
+  this.url = url;
+}
+
+Request.prototype.get = function (onComplete) {
+  return fetch(this.url)
+    .then((res) => res.json());
+}
+
+
+module.exports = RequestHelper;
---
+++ src/models/films.js
@@ -0,0 +1,6 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Films = function () {
+  this.filmsData = [];
+}
